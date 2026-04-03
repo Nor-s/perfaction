ed45f5d835a14a0fd0167711fbdb0afc174bef19
nor-s <nors.nsg@gmail.com>
2026-04-03 17:18:57 +0900

    update exit condition

--- src/Example.h
+++ src/Example.h
@@ -229,7 +229,7 @@ namespace tvgexam
 				auto result = 1 / ema_dt;
 				mfps += result;
 				if (tickCnt % 10 == 0) printf("[%5d]: %0.2f / %0.2f fps\n", tickCnt, result, mfps / (tickCnt - 59));
-				if (tickCnt == 5000) {
+				if (tickCnt == 3000) {
 					exit(0);
 				}
 			}
