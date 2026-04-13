5c4c30759250cc2e5ae64ba6c19d135fb0bb5368
Nor-s <soongeon@lottiefiles.com>
2026-04-13 20:47:27 +0900

    update

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,8 @@
-meson setup builddir-feature --wipe -Dthorvg_subproject=thorvg-feature -Dthreads=true --optimization=s --buildtype=release
-meson compile -C builddir-feature
+meson setup builddir-nors --wipe -Dthorvg_subproject=thorvg-nors -Dthreads=true --optimization=s
+meson compile -C builddir-nors
 
-meson setup builddir-main --wipe -Dthorvg_subproject=thorvg-main -Dthreads=true --optimization=s --buildtype=release
+meson setup builddir-nors-false --wipe -Dthorvg_subproject=thorvg-nors -Dthreads=false --optimization=s
+meson compile -C builddir-nors-false
+
+meson setup builddir-main --wipe -Dthorvg_subproject=thorvg-main -Dthreads=true --optimization=s
 meson compile -C builddir-main
